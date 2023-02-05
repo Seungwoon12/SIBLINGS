f7d11f736026be612313bb18e771e1c850c3d2f6
Seungwoon <swdev678@gmail.com>
2023-02-05 12:21:37 +0900

    siblings 스프링 업로드

---
+++ siblings/src/main/java/hello/siblings/SiblingsApplication.java
@@ -0,0 +1,13 @@
+package hello.siblings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiblingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiblingsApplication.class, args);
+	}
+
+}
---
+++ siblings/src/test/java/hello/siblings/SiblingsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.siblings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiblingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
