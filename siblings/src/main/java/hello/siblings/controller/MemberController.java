00e35b08dbb9136ff46071070e1e2d9a4bc29fc2
Seungwoon <swdev678@gmail.com>
2023-03-04 23:25:03 +0900

    파일 업로드

--- siblings/src/main/java/hello/siblings/controller/MemberController.java
+++ siblings/src/main/java/hello/siblings/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.siblings.controller;
 import hello.siblings.entity.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
