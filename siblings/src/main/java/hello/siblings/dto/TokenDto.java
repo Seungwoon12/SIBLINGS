166f2c8830d183e0b9d528355df09ab8b6fe34dd
Seungwoon <swdev678@gmail.com>
2023-03-05 00:11:28 +0900

    스프링 소스 추가

---
+++ siblings/src/main/java/hello/siblings/dto/TokenDto.java
@@ -0,0 +1,17 @@
+package hello.siblings.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+// Token 정보를 Response할 때 사용하기 위함
+public class TokenDto {
+
+    private String grantType; // JWT에 대한 인증 타입. Bearer 사용
+    private String accessToken;
+    private String refreshToken;
+
+}
