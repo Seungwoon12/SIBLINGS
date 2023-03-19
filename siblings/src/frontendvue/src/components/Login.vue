5b731c0335fee352bef399621a71da0c2c01666a
kimara <kimara@naver.com>
2023-03-19 13:41:48 +0900

    로그인수정

--- siblings/src/frontendvue/src/components/Login.vue
+++ siblings/src/frontendvue/src/components/Login.vue
@@ -4,25 +4,26 @@
         <input type="text" placeholder="ID">
         <input type="password" placeholder="PW">
 
-        <div @click="googleLogin" style="cursor">구글로 로그인하기</div>
+        <a :href="url1">구글로 로그인하기</a>
     </div>
 </template>
 
 <script>
 export default {
     name: "Login",
+    created(){
+        console.log("확인 : "+this.url1);
+    },
     data() {
         return{
             params: {
                 client_id : process.env.VUE_APP_GOOGLE_CLIENT_ID
             },
+            url1 :  process.env.VUE_APP_BACKEND_URL + "/oauth2/authorization/google?redirect_uri=" + process.env.VUE_APP_REDIRECT_URI
         }
     },
     methods:{
-        googleLogin(){
-            let backendUrl = process.env.VUE_APP_BACKEND_URL + "/oauth2/authorization/google?redirect_uri=" + process.env.VUE_APP_REDIRECT_URI;
-            this.$router.go(backendUrl);
-        }
+        
     }
 }
 </script>
